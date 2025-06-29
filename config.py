489f7f8e199817c92c4968f10e085f57a96c464f
bot-dd <botdd445@gmail.com>
2025-06-29 21:07:50 +0600

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "7822720438")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://rohanahamed75:gt4RXJZ1mUtOh4Xv@mmtg.0ong5.mongodb.net/?retryWrites=true&w=majority&appName=mmtg")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Anime9009:RXowwVBhS9jhvvkkhgfc@anime.5tk2hgj.mongodb.net/?retryWrites=true&w=majority&appName=Anime")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluooo")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "0"))  # 0 means no expiry
